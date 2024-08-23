51f95bc92be1b525a58a9b5816a5d12c9e355f01
BeLieVer031 <sandyrajak031@gmail.com>
2024-08-23 18:55:16 +0530

    project started input|btn|div taught with qs

---
+++ Basics of JS/22-08-24.js
@@ -0,0 +1,85 @@
+let temp = 30;
+if (temp <= 30) {
+    console.log("hot")
+}
+else {
+    console.log("cold")
+}
+
+
+let isStudent = true;
+if (isStudent) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+
+let age = 16;
+if (age >= 18) {
+    console.log("adult")
+}
+else {
+    console.log("minor")
+}
+
+
+let year = 2025;
+if (year % 4 == 0) {
+    console.log("leap year ")
+}
+else {
+    console.log("not a leap year")
+}
+
+
+
+let Score = 55;
+if (Score >= 50) {
+    console.log("pass")
+}
+else {
+    console.log("fail")
+}
+
+
+let num = 20;
+if (num % 2 == 0) {
+    console.log("even")
+}
+else {
+    console.log("odd")
+}
+
+
+
+let value = 16;
+if (value <= 20 && value >= 10) {
+    console.log("within the range")
+}
+else {
+    console.log("out of range")
+}
+
+
+
+let income = 22000;
+if (income >= 25000) {
+    console.log("eligible")
+}
+else {
+    console.log("not eligible")
+}
+
+
+let amount = 103;
+if (amount >= 100) {
+    console.log("greater than 100")
+}
+else {
+    console.log("less than 100")
+}
+
+
+
