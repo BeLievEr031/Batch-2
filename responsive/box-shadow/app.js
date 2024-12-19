4924f764514bdfd502bc61659fef3c390059e4e0
BeLieVer031 <sandyrajak031@gmail.com>
2024-12-19 18:46:24 +0530

    add vRangev and Hrange

---
+++ responsive/box-shadow/app.js
@@ -0,0 +1,13 @@
+const vRange = document.querySelector("#v-range")
+const hRange = document.querySelector("#h-range")
+const box = document.querySelector(".box")
+vRange.addEventListener("input", function () {
+    console.log(vRange.value);
+    // box.style.boxShadow = `${vRange.value}px ${hRange.value}px black`
+    box.style.boxShadow = `${hRange.value}px ${vRange.value}px black`
+
+})
+
+hRange.addEventListener("input", function () {
+    box.style.boxShadow = `${hRange.value}px ${vRange.value}px black`
+})
