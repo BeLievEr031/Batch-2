dcb9f79910497f47632b99c357977319cf17a1a9
BeLieVer031 <sandyrajak031@gmail.com>
2025-01-21 19:32:48 +0530

    add funtion for practice

---
+++ Function_Practice/index.js
@@ -0,0 +1,64 @@
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+add(2, 4);
+
+function sub(num1, num2) {
+    console.log(num1 - num2);
+}
+
+sub(2, 4);
+
+
+function multi(num1, num2) {
+    console.log(num1 * num2);
+}
+
+multi(2, 4)
+
+
+function div(num1, num2) {
+    console.log(num1 / num2);
+}
+
+div(4, 2)
+
+function rem(num1, num2) {
+    console.log(num1 % num2);
+}
+
+rem(5, 10);
+
+function printName(name1, name2) {
+    console.log(name1 + name2);
+}
+
+printName("a", "b")
+
+function bmi(w, h) {
+    console.log(w / h * h);
+}
+
+bmi(47, 1.5)
+
+function per(obval, tval) {
+    console.log(obval / tval * 100);
+}
+
+per(400, 600)
+
+function avg(n1, n2, n3, n4) {
+    console.log((n1 + n2 + n3 + n4) / 4);
+}
+
+avg(1, 2, 3, 4)
+
+function intrest(p, r, t) {
+    console.log(p * r * t);
+}
+
+intrest(2500, 1.2, 12)
+
+
+
